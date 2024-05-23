d62dfe0346474d2a46873127ae6578bfacc82a13
Markas Pozerskis <markaspozerskis@Markass-Air.lan>
2024-05-23 20:35:06 +0300

    1 change

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-var timeLeft = 30;
-    var elem = document.getElementById('some_div');
-    
-    var timerId = setInterval(countdown, 1000);
-    
-    function countdown() {
-      if (timeLeft == -1) {
-        clearTimeout(timerId);
-        doSomething();
-      } else {
-        elem.innerHTML = timeLeft + ' seconds remaining';
-        timeLeft--;
-      }
-    }
+let timeLeft;
+let element = document.getElementById("some-div");
+let rstbtn=document.getElementById("reset");
+let timerId;
+let stopped=false;
+function countdown() {
+  if (timeLeft > 0) {
+    timeLeft--;
+    element.innerHTML = parseInt(timeLeft/60) + ' minutes ' + timeLeft%60 + ' seconds remaining';
+  }
+}
+
+function stop(){
+  if(stopped){
+    timerId = setInterval(countdown, 1000);
+    document.getElementById("stop").innerHTML="STOP";
+  }else{
+    clearInterval(timerId);
+    document.getElementById("stop").innerHTML="RESUME";
+  }
+  stopped=!stopped;
+}
+
+
+function reset(){
+  timeLeft=1500;
+  timeLeft++;
+  countdown();
+  timerId = setInterval(countdown, 1000);
+  console.log("RESET INITIATED");
+  rstbtn.innerHTML = "RESET";
+}
